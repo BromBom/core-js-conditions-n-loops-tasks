033f1a7677b8ecc25da40d7b5fd6bac57a3b7355
Ihar Levachkou <pokerlis@gmail.com>
2024-01-18 16:18:04 +0300

    feat: add solution to canQueenCaptureKing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -66,8 +66,15 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x || queen.y === king.y) return true;
+  for (let i = 1; i < 8; i += 1) {
+    if (queen.x === king.x + i && queen.y === king.y + i) return true;
+    if (queen.x === king.x - i && queen.y === king.y - i) return true;
+    if (queen.x === king.x - i && queen.y === king.y + i) return true;
+    if (queen.x === king.x + i && queen.y === king.y - i) return true;
+  }
+  return false;
 }
 
 /**
